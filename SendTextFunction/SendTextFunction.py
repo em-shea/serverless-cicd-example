47d84b9d99b5112365ca4674d14adfedea640ca9
em-shea <c.emilyshea@gmail.com>
2020-05-17 18:10:20 -0700

    initial commit

---
+++ SendTextFunction/SendTextFunction.py
@@ -0,0 +1,23 @@
+import os
+import json
+import boto3
+
+sns_client = boto3.client('sns')
+
+def lambda_handler(event, context):
+
+  text_message = "Congrats! You deployed this application using a CI/CD pipeline."
+
+  publish_sns_update(text_message)
+
+  # add error handling, phone number last four digits
+
+def publish_sns_update(text_message):
+
+  response = sns_client.publish(
+      TargetArn = os.environ['SNS_TOPIC_ARN'], 
+      Message=json.dumps({'default': text_message}),
+      MessageStructure='json'
+  )
+
+  return response
